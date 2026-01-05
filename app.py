91636804233d52ae37c17bf8f1265a5b86027ce7
K0c12 <73248287+K0c12@users.noreply.github.com>
2026-01-05 17:30:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import pandas as pd
+# Importa a função do outro arquivo
+from coleta_dados_loteca import buscar_dados_vovoteca 
+
+# Chama a função
+df = buscar_dados_vovoteca()
 import os
 import json
 import math
@@ -155,4 +161,5 @@ def processar():
 
 if __name__ == '__main__':
     if not os.path.exists(DB_FILE): salvar_banco_escudos({})
+
     app.run(host='0.0.0.0', port=8080)
